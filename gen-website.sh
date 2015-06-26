d029ac3b2fda495c39e13d5586ca734414f6216b
André Miranda <andremiramor@gmail.com>
2015-06-26 16:45:37 -0300

    Add code from other repo

---
+++ gen-website.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+printf "Copiando pdfs..."
+cp ../hinario-espirita/Hinário.pdf .
+cp ../hinario-espirita/Cifras.pdf .
+cp ../hinario-espirita/Transparências.pdf .
+cp ../hinario-espirita/Eslaides.pdf .
+cp ../hinario-espirita/Capa/Cartaz.pdf .
+printf "pronto.\n"
+
+printf "Copiando midis e oggs..."
+cp ../hinario-espirita/LilyPond/*.pdf partituras/data/
+cp ../hinario-espirita/LilyPond/*.ogg partituras/data/
+cp ../hinario-espirita/LilyPond/*.midi partituras/data/
+printf "pronto.\n"
+
+printf "Compilando Sass... "
+sass style.sass style.css
+printf "pronto.\n"
+
+printf "Compilando HAML... "
+haml index.haml index.html
+haml partituras/index.haml partituras/index.html
+printf "pronto.\n"
+
+printf "Removendo arquivos sujeira... "
+rm style.css.map
+rm -rf .sass-cache/
+printf "pronto.\n"
