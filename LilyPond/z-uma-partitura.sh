8572289fc7c3964d0ff4c227f6859fd3f89fd95e
André Miranda <andremiramor@gmail.com>
2015-06-26 00:43:19 -0300

    Remove Partituras.pdf

--- LilyPond/z-uma-partitura.sh
+++ LilyPond/z-uma-partitura.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-data_folder=../../hinario-espirita.github.io/partituras/data
-
 fname=${1%.*}
 printf "Gerando '$fname.pdf'... "
 lilypond "$1"
-timidity "$fname.midi" -Ov
-mv "$fname.ogg" $data_folder
-mv "$fname.midi" $data_folder
-mv "$fname.pdf" $data_folder
-pdfunite ../Capa/Capa.pdf ../../hinario-espirita.github.io/partituras/data/*.pdf ../Partituras.pdf
+timidity "${1%.*}.midi" -Ov
 printf "pronto.\n"
